a18e0e3bc01858f1ccf6dd3ead95f4d289612ec2
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-15 12:52:12 +0200

    test

--- main.tf
+++ main.tf
@@ -1,32 +1,17 @@
-provider "azurerm" {
-  features {}
- 
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.47.0"
-    }
-  }
-}
-
-
 resource "azurerm_resource_group" "aks-rg" {
-  name     = "aks_tf_test"
-  location = "EastUS2"
+  name     = var.resource_group_name
+  location = var.location
 }
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "devops-test-aks"
-  kubernetes_version  = "1.24.9"
-  location            = "EastUS2"
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  location            = var.location
   resource_group_name = azurerm_resource_group.aks-rg.name
-  dns_prefix          = "devops-test-aks"
+  dns_prefix          = var.cluster_name
 
   default_node_pool {
     name                = "system"
-    node_count          = 2
+    node_count          = var.system_node_count
     vm_size             = "Standard_DS2_v2"
     type                = "VirtualMachineScaleSets"
     enable_auto_scaling = false
@@ -35,7 +20,5 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
-
-  
 }
 
