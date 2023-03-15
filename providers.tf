83a092ef8753f5e1090d4ff58b211a39786e988b
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-16 01:07:51 +0200

    test

--- providers.tf
+++ providers.tf
@@ -11,10 +11,11 @@ terraform {
       version = "3.47.0"
     }
   }
-  #   backend "azurerm" {
-  #   resource_group_name  = "tfstate"
-  #   storage_account_name = "tfstate12334"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
+    backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate12334"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    access_key = "xu+ou9EKTUv24/lMVn3Tcq5f98isQScLqDF1/RDYhydFjT//K30NlYyINEp0M1/9OsqpzHyHB5e0+AStKHDxaA=="
+  }
 }
