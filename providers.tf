6a72015f967fd84982e69754b609bb194bd247ff
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-15 23:21:56 +0200

    test

--- providers.tf
+++ providers.tf
@@ -2,3 +2,17 @@ provider "azurerm" {
   features {}
  
 }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.47.0"
+    }
+  }
+    backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate5617"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
