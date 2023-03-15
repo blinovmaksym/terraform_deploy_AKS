2ac38a60e06973771440535d321b7f6f47a6e423
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-16 00:33:39 +0200

    test

--- create_storage.sh
+++ create_storage.sh
@@ -5,6 +5,6 @@ CONTAINER_NAME=tfstate
 # Create resource group
 az group create --name $RESOURCE_GROUP_NAME --location eastus2
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS  --https-only true
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
