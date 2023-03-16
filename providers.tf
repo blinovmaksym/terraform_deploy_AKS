413f9c306ca37e5579a745d312fd15a79001141d
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-16 02:05:07 +0200

    test

--- providers.tf
+++ providers.tf
@@ -1,9 +1,6 @@
 provider "azurerm" {
   features {}
-  client_id       = "cfb628af-80db-4f46-adb9-690c7bebf8fb"
-  client_secret   = "j1Y8Q~Q4EzoMBxA6QORZaLSoSRTceThAUXJlpcFj"
-  tenant_id       = "b5c47142-c960-43f4-b337-211e1c4a1bb7"
-  subscription_id = "fd8b1619-2a10-415d-b7db-a87dfb217eb2"
+  
 }
 terraform {
   
@@ -22,6 +19,10 @@ terraform {
     storage_account_name = "tfstate12334"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    client_id       = "cfb628af-80db-4f46-adb9-690c7bebf8fb"
+    client_secret   = "j1Y8Q~Q4EzoMBxA6QORZaLSoSRTceThAUXJlpcFj"
+    tenant_id       = "b5c47142-c960-43f4-b337-211e1c4a1bb7"
+    subscription_id = "fd8b1619-2a10-415d-b7db-a87dfb217eb2"
     
   }
 }
