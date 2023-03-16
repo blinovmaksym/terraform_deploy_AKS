55277c1ad662b049e076a22250862f38803577f3
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-16 02:04:06 +0200

    test

--- providers.tf
+++ providers.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
   features {}
- 
+  client_id       = "cfb628af-80db-4f46-adb9-690c7bebf8fb"
+  client_secret   = "j1Y8Q~Q4EzoMBxA6QORZaLSoSRTceThAUXJlpcFj"
+  tenant_id       = "b5c47142-c960-43f4-b337-211e1c4a1bb7"
+  subscription_id = "fd8b1619-2a10-415d-b7db-a87dfb217eb2"
 }
 terraform {
   
@@ -9,8 +12,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.47.0"
-      clienr_id="cfb628af-80db-4f46-adb9-690c7bebf8fb"
-      client_secret="j1Y8Q~Q4EzoMBxA6QORZaLSoSRTceThAUXJlpcFj"
+      
 
     }
     
