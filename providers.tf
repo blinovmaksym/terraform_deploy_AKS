ad5777c7d9c54a0b3ebce28552f281b0ca84e780
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-16 02:24:38 +0200

    test

--- providers.tf
+++ providers.tf
@@ -19,10 +19,5 @@ terraform {
     storage_account_name = "tfstate16766"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    client_id       = "cfb628af-80db-4f46-adb9-690c7bebf8fb"
-    client_secret   = "j1Y8Q~Q4EzoMBxA6QORZaLSoSRTceThAUXJlpcFj"
-    tenant_id       = "b5c47142-c960-43f4-b337-211e1c4a1bb7"
-    subscription_id = "fd8b1619-2a10-415d-b7db-a87dfb217eb2"
-    
-  }
+   }
 }
