91322ce98df437c83404ed0531fda726f2c2a5f5
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-19 13:56:50 +0200

    test

--- terraform-azure/scripts/create_storage.sh
+++ terraform-azure/scripts/create_storage.sh
@@ -12,3 +12,5 @@ az group create --name $RESOURCE_GROUP_NAME --location eastus2
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS  --https-only true
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+
+slepp 30s
