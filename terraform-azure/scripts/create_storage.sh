a5c3e9272a86a842deeab4a153a6560e1417d213
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-19 14:32:45 +0200

    test

--- terraform-azure/scripts/create_storage.sh
+++ terraform-azure/scripts/create_storage.sh
@@ -1,19 +1,22 @@
-!/bin/bash
-RESOURCE_GROUP_NAME=tfstate
-STORAGE_ACCOUNT_NAME=tfstateitsprout
-CONTAINER_NAME=tfstate
+#!/bin/bash
 
- # Check if storage account exists
-if ! az storage account show --name $STORAGE_ACCOUNT_NAME --query id --output tsv >/dev/null 2>&1; then
-# Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location eastus2
-# Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS  --https-only true
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
-sleep 30s
-else
-echo "Storage already exist"
-sleep 5s
+echo "Hello"
+sleep 10s
+# RESOURCE_GROUP_NAME=tfstate
+# STORAGE_ACCOUNT_NAME=tfstateitsprout
+# CONTAINER_NAME=tfstate
 
-fi
+#  # Check if storage account exists
+# if ! az storage account show --name $STORAGE_ACCOUNT_NAME --query id --output tsv >/dev/null 2>&1; then
+# # Create resource group
+# az group create --name $RESOURCE_GROUP_NAME --location eastus2
+# # Create storage account
+# az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS  --https-only true
+# # Create blob container
+# az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+# sleep 30s
+# else
+# echo "Storage already exist"
+# sleep 5s
+
+# fi
