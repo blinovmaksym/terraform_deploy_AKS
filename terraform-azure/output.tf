dea0e5fcb0fcd13a406fefe96f7b7f6c035da3e4
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-25 19:08:04 +0200

    test

--- terraform-azure/output.tf
+++ terraform-azure/output.tf
@@ -1,9 +1,8 @@
-resource "local_file" "kubeconfig" {
-depends_on = [azurerm_kubernetes_cluster.aks]
-filename = "kubeconfig"
-content = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.product.kube_config_raw
 
+  sensitive = true
+}
 
 output "resource_group_name" {
   value = azurerm_resource_group.aks-rg.name
