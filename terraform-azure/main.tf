7b27877eeb8b489167894e6a9ddd80bfa6325f89
Maksym Blinov <m.blinov@it-sprout.org>
2023-03-25 17:30:39 +0200

    test

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -23,32 +23,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 }
-resource "kubernetes_deployment" "hello-world" {
-  metadata {
-    name = "hello-world"
-  }
-  spec {
-    selector {
-      match_labels = {
-        app = "hello-world"
-      }
-    }
-    replicas = 1
-    template {
-      metadata {
-        labels = {
-          app = "hello-world"
-        }
-      }
-      spec {
-        container {
-          name  = "hello-world"
-          image = "hello-world:latest"
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
