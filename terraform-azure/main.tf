05213d2fa7bb47f62ae404f03be69fe2878cfa88
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-17 15:23:51 +0300

    test

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -27,8 +27,8 @@ resource "local_file" "kubeconfig" {
   content  = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
 # Create MySql Server 
-resource "azurerm_mysql_server" "product" {
-  name                = "mysql-wp"
+resource "azurerm_mysql_server" "aks-bd" {
+  name                = "mysql-wpmax"
   location            = azurerm_resource_group.aks-rg.location
   resource_group_name = azurerm_resource_group.aks-rg.name
 
