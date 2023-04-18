c5791c2bb644d281d69d086fa81c872af3fe4e73
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-18 13:31:12 +0300

    destroy

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -47,6 +47,8 @@ resource "local_file" "kubeconfig" {
 #   ssl_enforcement_enabled           = false
 #   ssl_minimal_tls_version_enforced  = "TLS1_2"
 # }
+
+
 resource "azurerm_mssql_server" "aks-bd_sprout" {
   name                         = "mssqlserver123"
   resource_group_name          = azurerm_resource_group.aks-rg.name
