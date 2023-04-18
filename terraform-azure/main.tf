eacbd331a75da09e7d13979184f29c19fa44be04
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-18 10:33:28 +0300

    test

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -26,24 +26,49 @@ resource "local_file" "kubeconfig" {
   filename = "${path.module}/kubeconfig"
   content  = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
-# Create MySql Server 
-resource "azurerm_mysql_server" "aks-bd" {
-  name                = "mysql-wpmax"
-  location            = azurerm_resource_group.aks-rg.location
-  resource_group_name = azurerm_resource_group.aks-rg.name
+# # Create MySql Server 
+# resource "azurerm_mysql_server" "aks-bd" {
+#   name                = "mysql-wpmax"
+#   location            = azurerm_resource_group.aks-rg.location
+#   resource_group_name = azurerm_resource_group.aks-rg.name
 
+#   administrator_login          = "mysqladmin"
+#   administrator_login_password = "1-qwerty"
+
+#   sku_name   = "B_Gen5_2"
+#   storage_mb = 5120
+#   version    = "5.7"
+
+#   auto_grow_enabled                 = true
+#   backup_retention_days             = 30
+#   geo_redundant_backup_enabled      = false
+#   infrastructure_encryption_enabled = false
+#   public_network_access_enabled     = true
+#   ssl_enforcement_enabled           = false
+#   ssl_minimal_tls_version_enforced  = "TLS1_2"
+# }
+resource "azurerm_mssql_server" "aks-bd" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.aks-rg.name
+  location                     = azurerm_resource_group.aks-rg.location
+  version                      = "12.0"
   administrator_login          = "mysqladmin"
   administrator_login_password = "1-qwerty"
+  minimum_tls_version          = "1.2"
 
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
+  # azuread_administrator {
+  #   login_username = "AzureAD Admin"
+  #   object_id      = "00000000-0000-0000-0000-000000000000"
+  # }
 
-  auto_grow_enabled                 = true
-  backup_retention_days             = 30
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  # tags = {
+  #   environment = "production"
+  # }
+}
+resource "azurerm_mysql_firewall_rule" "bd_role" {
+  name                = "AllowAllIPs"
+  resource_group_name = azurerm_resource_group.aks-rg.name
+  server_name         = azurerm_mssql_server.aks-bd.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
