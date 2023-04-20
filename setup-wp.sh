11b88c66891cceebcdbec216afa5a32e36fa5a4d
blinovmaxim <blinov.maxim@gmail.com>
2023-04-20 23:30:18 +0300

    test

--- setup-wp.sh
+++ setup-wp.sh
@@ -30,5 +30,6 @@ else
     wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
     wp --allow-root --path=/var/www/html theme install astra  && \
     wp --allow-root --path=/var/www/html theme activate astra 
+
     
 fi
