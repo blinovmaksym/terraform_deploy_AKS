64c26351cff6d676cc98ea0c895090bcaf9ca9f2
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-20 17:55:32 +0300

    test

--- setup-wp.sh
+++ setup-wp.sh
@@ -8,7 +8,11 @@ mysql -h $SERVER_NAME -u $USER_NAME -p$PASSWORD -P 3306 -e "\
     INSERT IGNORE INTO my_table (id, name) VALUES (1, 'John'), (2, 'Jane'), (3, 'Joe');" 
 if [ ! -f /var/www/html/wp-config.php ]; then    
     # Configure WP-CLI and install plugins and themes
-    wp core download --path=/var/www/html --allow-root && \
+    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
+    wp --allow-root --path=/var/www/html theme install twentytwentytwo && \
+    wp --allow-root --path=/var/www/html theme activate twentytwentytwo
+    wp core download --path=/var/www/html --allow-root 
+else    
     wp --allow-root --path=/var/www/html config create \
       --dbhost=mysql-wpmax.mysql.database.azure.com \
       --dbname=test_manifest12 \
@@ -20,8 +24,5 @@ if [ ! -f /var/www/html/wp-config.php ]; then
       --title=WPsite \
       --admin_user=admin \
       --admin_password=admin \
-      --admin_email=admin@example.com  &&\
-    wp --allow-root --path=/var/www/html plugin install contact-form-7 --activate && \
-    wp --allow-root --path=/var/www/html theme install twentytwentytwo && \
-    wp --allow-root --path=/var/www/html theme activate twentytwentytwo
+      --admin_email=admin@example.com
 fi
