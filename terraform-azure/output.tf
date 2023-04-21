8e02a96d6c72b00b502cd6f42b343df6ed9b2b64
Maksym Blinov <m.blinov@it-sprout.org>
2023-04-21 14:22:47 +0300

    build

--- terraform-azure/output.tf
+++ terraform-azure/output.tf
@@ -8,5 +8,5 @@ output "resource_group_name" {
   value = azurerm_resource_group.aks-rg.name
 }
 output "wordpress_url" {
-  value = azurerm_dns_cname_record.aks-dns-zone.record
+  value = "https://${azurerm_dns_cname_record.aks-dns-zone.cname}"
 }
