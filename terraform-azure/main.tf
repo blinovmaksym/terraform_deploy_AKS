a5f9b488b4851e7d227c4bd9a48313642af9047f
blinovmaxim <blinov.maxim@gmail.com>
2023-04-21 21:45:55 +0300

    destroy

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -65,6 +65,7 @@ resource "azurerm_mysql_firewall_rule" "aks-bd_sprout" {
     name                = "LoadBalancer"
     location            = azurerm_resource_group.aks-rg.location
     resource_group_name = azurerm_resource_group.aks-rg.name
+    
     frontend_ip_configuration {
       name                 = "LoadBalancer_lb_public_ip"
       public_ip_address_id = azurerm_public_ip.aks-pip.id
