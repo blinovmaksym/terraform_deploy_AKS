ed7f2ed96fb0f20680a85a1fe3a450fd8794170f
blinovmaxim <blinov.maxim@gmail.com>
2023-04-30 14:02:30 +0300

    test

--- terraform-azure/scripts/install-tekton.sh
+++ terraform-azure/scripts/install-tekton.sh
@@ -6,5 +6,8 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline
 # Install Tekton Triggers
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 
-# Install Tekton Dashboard
-kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
+# Install Tekton Dashboard with LoadBalancer type service
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml \
+--overwrite=true \
+--selector app=tekton-dashboard \
+--set service.type=LoadBalancer
