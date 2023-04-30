3a3fb994d222616d3ef54d460cbadbd407eb914e
blinovmaxim <blinov.maxim@gmail.com>
2023-04-30 18:32:35 +0300

    test

--- terraform-azure/scripts/install-tekton.sh
+++ terraform-azure/scripts/install-tekton.sh
@@ -18,40 +18,42 @@ ssh-keygen -t rsa -b 4096 -C "tekton@tekton.dev" -f tekton_key
 ls
 # Get the public key contents
 public_key=$(cat tekton_key.pub)
+cat tekton_key
+cat  public_key
 # Create a new SSH key on GitHub
-curl -X POST -H "Authorization: token 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" -d '{"title":"Tekton SSH Key","key":"'"${public_key}"'"}' https://api.github.com/blinovmaksym/keys
+curl -X POST -H "Authorization: token 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" -d '{"title":"Tekton SSH Key","key":"'"${public_key}"'"}' https://api.github.com/user/keys
 
-# curl -X POST -H "Authorization: token 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" -d '{"title":"Tekton SSH Key","key":"'${public_key}'"}' https://api.github.com/user/keys
-# create secret YAML from contents
-cat tekton_key | base64 -w 0
-cat > tekton-git-ssh-secret.yaml << EOM
-apiVersion: v1
-kind: Secret
-metadata:
-  name: git-ssh-key
-  namespace: tekton-pipelines
-  annotations:
-    tekton.dev/git-0: github.com
-type: kubernetes.io/ssh-auth
-data:
-  ssh-privatekey: <base64 data>
----
-EOM
+# # curl -X POST -H "Authorization: token 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" -d '{"title":"Tekton SSH Key","key":"'${public_key}'"}' https://api.github.com/user/keys
+# # create secret YAML from contents
+# cat tekton_key | base64 -w 0
+# cat > tekton-git-ssh-secret.yaml << EOM
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: git-ssh-key
+#   namespace: tekton-pipelines
+#   annotations:
+#     tekton.dev/git-0: github.com
+# type: kubernetes.io/ssh-auth
+# data:
+#   ssh-privatekey: <base64 data>
+# ---
+# EOM
 
-kubectl apply -f tekton-git-ssh-secret.yaml
+# kubectl apply -f tekton-git-ssh-secret.yaml
 
-# create your Docker registry secret, for example:
-cat ~/.docker/config.json | base64 -w 0
-cat > regsecret.yaml << EOM
-kind: Secret
-apiVersion: v1
-metadata:
-  name: regsecret
-data:
-  .dockercfg: <base 64 data>
-type: kubernetes.io/dockercfg
-EOM
+# # create your Docker registry secret, for example:
+# cat ~/.docker/config.json | base64 -w 0
+# cat > regsecret.yaml << EOM
+# kind: Secret
+# apiVersion: v1
+# metadata:
+#   name: regsecret
+# data:
+#   .dockercfg: <base 64 data>
+# type: kubernetes.io/dockercfg
+# EOM
 
-kubectl apply -n tekton-pipelines -f regsecret.yaml
+# kubectl apply -n tekton-pipelines -f regsecret.yaml
 
-kubectl apply -f tekton/
+# kubectl apply -f tekton/
