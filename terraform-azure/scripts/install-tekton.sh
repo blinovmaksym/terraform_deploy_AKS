d491b685df8b66403bcd1003f206f8a21aa2fc43
blinovmaxim <blinov.maxim@gmail.com>
2023-04-30 13:54:13 +0300

    test

---
+++ terraform-azure/scripts/install-tekton.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Tekton Pipelines
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+# Install Tekton Triggers
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+
+# Install Tekton Dashboard
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
