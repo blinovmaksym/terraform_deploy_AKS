f5b9fe86d02ef88fe8e5259e9f73e0ab61a6d97f
Maksym Blinov <m.blinov@it-sprout.org>
2023-05-03 14:26:32 +0300

    test

--- terraform-azure/scripts/install-tekton.sh
+++ terraform-azure/scripts/install-tekton.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install Tekton CLI
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.30.1/tkn_0.30.1_Darwin_all.tar.gz
+# Extract tkn to your PATH (e.g. /usr/local/bin)
+sudo tar xvzf tkn_0.30.1_Darwin_all.tar.gz -C /usr/local/bin tkn
+
 # Install Tekton Pipelines
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
@@ -15,6 +20,10 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboar
 
 # Update Tekton Dashboard service to use LoadBalancer
 kubectl patch service tekton-dashboard -n tekton-pipelines --type='json' -p '[{"op":"replace","path":"/spec/type","value":"LoadBalancer"}]'
+
+#Install tasks  from Tekton Hub
+tkn hub install task git-clone && tkn hub install task buildah
+
 
 if ! curl -s -H "Authorization: token $TOKEN_TEKTON" https://api.github.com/user/keys | grep -q "Tekton SSH Key"; then
     ssh-keygen -t rsa -b 4096 -C "tekton@tekton.dev" -f tekton_key
